d6753c2f2f1f24c6617e52264218f99178a06c04
Angelo Solofonirina <angelosolofonirina@gmail.com>
2023-04-22 18:22:54 +0300

    Commentaire ajouté sur le fichier index.js dans le dossier 'src'

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+//Commantaire du 22 Avril 2023 par Angelo
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
